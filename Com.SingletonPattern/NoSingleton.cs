28368377e2956ea1e4195780184c73b1c9d41bc5
willieni <niliwei2@163.com>
2015-07-10 22:04:26 +0800

    单例模式

---
+++ Com.SingletonPattern/NoSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.SingletonPattern
+{
+    /// <summary>
+    /// 功能：不用单件模式，直接返回一个对象
+    /// 编写：WillieNi
+    /// 日期：2015年07月10日
+    /// </summary>
+    public class NoSingleton
+    {
+        public NoSingleton()
+        {
+
+        }
+    }
+}
--- SingletonClient/Properties/AssemblyInfo.cs
+++ SingletonClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SigletonPattern")]
+[assembly: AssemblyTitle("SingletonClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SigletonPattern")]
+[assembly: AssemblyProduct("SingletonClient")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("92b65fd3-5a1a-47d1-bb79-9f4c8bb86eff")]
+[assembly: Guid("818fdff0-e433-4293-8aea-34b3ee7e8613")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
